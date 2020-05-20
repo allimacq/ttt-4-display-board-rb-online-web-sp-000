711914f2fdf213808a1e3c54920201c841ce4ed5
Allison MacQueen <allimacq@gmail.com>
2020-05-20 21:45:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,8 @@
 def display_board(board = [" "," "," "," "," "," "," "," "," "])
   row_one = " #{board[0]}  | #{board[1]} | #{board[2]} "
   dashes = "-----------"
-  row_two = " #{board[3]} ", " | ", " #{board[4]} ", " | ", " #{board[5]} "
-  row_three = " #{board[6]} ", " | ", " #{board[7]} ", " | ", " #{board[8]} "
+  row_two = " #{board[3]} | #{board[4]}  | #{board[5]} "
+  row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
   puts row_one
   puts dashes
   puts row_two
