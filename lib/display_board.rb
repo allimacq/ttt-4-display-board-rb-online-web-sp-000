2506505bc95762fd3ccf844f8c6261a60dd714e4
Allison MacQueen <allimacq@gmail.com>
2020-05-20 21:24:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board = [" "," "," "," "," "," "," "," "," "])
-  odd_row = [""]
+def display_board(board = [" "," "," "," "," "," "," "," "," )
+  odd_row = ["   ", " | ", "   ", " | ", "   "]
+  even_row = ["-----------"]
